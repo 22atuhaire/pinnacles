6eeb7a065e0b36e7e3f99642e9dab9e1331060a8
22atuhaire <atuhaire.abiudi@student.utamu.ac.ug>
2025-03-30 20:49:08 +0300

    initialize Django apps and create basic structure for auth, core, about, posts, members, donation, activities, and pinnacles applications

---
+++ activities_app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# activities/views.py
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Activity
+from members.models import Profile  # Import Profile from the members app
+
+@login_required  # Re-enable the decorator
+def activities_list(request):
+    activities = Activity.objects.all().order_by('-date')  # Fetch activities sorted by date
+    
+    # Ensure the logged-in user has a profile
+    current_user_profile = getattr(request.user, 'profile', None)
+    if not current_user_profile:
+        current_user_profile = Profile.objects.get_or_create(user=request.user)[0]
+    current_user_profile.refresh_from_db()  # Force reload from the database
+    
+    return render(request, 'activities.html', {
+        'activities': activities,
+        'current_user_profile': current_user_profile
+    })
