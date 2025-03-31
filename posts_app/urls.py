6e99b7bf881163111e89752b4dca8fadd451f203
22atuhaire <atuhaire.abiudi@student.utamu.ac.ug>
2025-03-31 17:46:22 +0300

    Rename app_name from 'posts_app' to 'news' for improved clarity in URL namespace

--- posts_app/urls.py
+++ posts_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'posts_app'
+app_name = 'news'
 # The 'app_name' variable is used to create namespaced URLs for the app.
 urlpatterns = [
     path('', views.news_list_view, name='news_list'),
