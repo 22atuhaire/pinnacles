e80d2a8fede121e91fa436b1843d6b36761654fa
22atuhaire <atuhaire.abiudi@student.utamu.ac.ug>
2025-04-09 22:28:37 +0300

    update ALLOWED_HOSTS to use environment variable for better configuration

--- pinnacles/settings.py
+++ pinnacles/settings.py
@@ -14,10 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-# 'django-insecure-y1279l)7tvh-w@)$=jir*s1hhc5%-!hgx4uwlj_iz@@%9cz$9f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-ALLOWED_HOSTS = ['pinnacles.onrender.com']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "*").split(",")
 
 
 
